3a168a2352a3d7ce85aa4f5f3681a31db88b6e09
shixing <15365011589@163.com>
2020-10-29 22:10:33 +0800

    Initialize Project

--- TS_Account/Account-Starter/src/main/java/com/selfpractice/moudles/account/AccountStarter.java
+++ TS_Account/Account-Starter/src/main/java/com/selfpractice/moudles/account/AccountStarter.java
@@ -1,4 +1,11 @@
 package com.selfpractice.moudles.account;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class AccountStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountStarter.class,args);
+    }
 }
